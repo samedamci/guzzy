1b5738b7e9432900b932dd09fc32c6decb82c1e3
samedamci <samedamci@disroot.org>
2021-08-26 22:46:35 +0200

    Move main code to __init__.py to avoid Traceback errors

--- guzzy/__main__.py
+++ guzzy/__main__.py
@@ -1,16 +1,4 @@
-from os import listdir, path
-from argparse import ArgumentParser
-from subprocess import call
+from . import main
 
-dir_path = path.join(path.dirname(path.realpath(__file__)), "commands")
-commands = [x.replace(".sh", "") for x in listdir(dir_path)]
-
-parser = ArgumentParser(
-    prog="guzzy",
-    description="Interactive CLI git client inside fzf.",
-    allow_abbrev=False,
-)
-parser.add_argument("command", choices=commands)
-args = parser.parse_args()
-
-call(path.join(dir_path, f"{args.command}.sh"), shell=True)
+if __name__ == "__main__":
+    main()
