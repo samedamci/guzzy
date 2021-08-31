4d19b1b8a970f116e23ab45ae1fcdec1a16e148a
samedamci <samedamci@disroot.org>
2021-09-01 00:16:58 +0200

    Add colorized output to files command

--- guzzy/commands/files.sh
+++ guzzy/commands/files.sh
@@ -2,10 +2,11 @@
 
 [ "$1" ] && commit_hash="$1" || commit_hash="HEAD"
 
-commit="git --no-pager show -q $commit_hash"
-limiter="seq -s_ 150|tr -d '[:digit:]'"
+commit="git --no-pager log --color=always -n 1 $commit_hash"
+delimiter="seq -s_ 150 | tr -d '[:digit:]'"
 file_content="git show $commit_hash:{}"
+pager="bat --style numbers --decorations always --file-name {}"
 
 git ls-tree -r --name-only "$commit_hash" |
-    fzf --ansi --preview "$commit && $limiter && $file_content" \
+    fzf --ansi --preview "$commit && $delimiter && $file_content | $pager" \
         --preview-window=right:70%
