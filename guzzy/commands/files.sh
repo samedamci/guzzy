c4b8140948da2f03b919db87b2621a036bcf9db4
samedamci <samedamci@disroot.org>
2021-08-31 13:55:36 +0200

    Improve limiter in files command

--- guzzy/commands/files.sh
+++ guzzy/commands/files.sh
@@ -3,9 +3,9 @@
 [ "$1" ] && commit_hash="$1" || commit_hash="HEAD"
 
 commit="git --no-pager show -q $commit_hash"
-limiter="-------------------------------------------------"
+limiter="seq -s_ 150|tr -d '[:digit:]'"
 file_content="git show $commit_hash:{}"
 
 git ls-tree -r --name-only "$commit_hash" |
-    fzf --ansi --preview "$commit && echo $limiter && $file_content" \
+    fzf --ansi --preview "$commit && $limiter && $file_content" \
         --preview-window=right:70%
