dec99a2431797f94fc83c67b786880097fc37c2e
samedamci <samedamci@disroot.org>
2021-08-31 12:34:38 +0200

    Add built-in binding for files command to log command

--- guzzy/commands/log.sh
+++ guzzy/commands/log.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+CWD="$(dirname "$(readlink -f "$0")")"
+
 show_diff="git log --color=always -n 1 {1}; seq -s_ 150|tr -d '[:digit:]' && \
   git diff --color=always {1}~ {1}"
 
 git log --color=always --format="%C(auto)%h %s" | fzf --ansi \
-  --preview="$show_diff" --preview-window=right:55%
+  --preview="$show_diff" --preview-window=right:55% \
+  --bind "ctrl-t:execute($CWD/files.sh {1})"
